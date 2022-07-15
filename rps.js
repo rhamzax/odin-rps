e8b9180ea94de58f26f13b5e1a9571f28acefd27
Hamza Amar <rhamzax@gmail.com>
2022-07-15 01:42:25 -0600

    Add a play again button

--- rps.js
+++ rps.js
@@ -1,3 +1,12 @@
+const buttons = document.querySelectorAll('input');
+const event = document.querySelector('.event');
+const userScore = document.querySelector('.user-score');
+const compScore = document.querySelector('.comp-score');
+const playAgain = document.querySelector('.playAgain');
+
+buttons.forEach(button => button.addEventListener('click', playRound));
+playAgain.addEventListener('click', restartGame);
+
 function computerPlay(){
     random = Math.floor(Math.random() * 3);
     if(random === 0){
@@ -10,71 +19,73 @@ function computerPlay(){
         return "scissors";
     }
 }
-function updateScore(winner){
-    const userScore = document.querySelector('.user-score');
-    const compScore = document.querySelector('.comp-score');
+function checkGameOver(winner){
     if(userScore.textContent == 5 || compScore.textContent == 5){
-        reset(userScore, compScore, winner);
+        reset(winner);
     }
+}
+function updateScore(winner){
     if(winner === "User"){
         userScore.textContent = parseInt(userScore.textContent) + 1;
+        checkGameOver(winner);
     }
     else if(winner === "Computer"){
         compScore.textContent = parseInt(compScore.textContent) + 1;
+        checkGameOver(winner);
     }
+    
 }
 function playRound(e){
     let computerSelection = computerPlay();
     let playerSelection = e.target.value;
-    const event = document.querySelector('.event');
+    
 
     if (playerSelection === computerSelection){
         event.textContent = "Draw"
+        updateScore("Draw");
     }
     else if (playerSelection === "paper"){
         if(computerSelection === "rock"){
-            updateScore("User");
             event.textContent = "You Win! Paper beats Rock";
+            updateScore("User");
         }
         else if(computerSelection === "scissors"){
-            updateScore("Computer");
             event.textContent = "You Lose! Scissors beats Paper";
+            updateScore("Computer");
         }
     }
     else if (playerSelection === "scissors"){
         if(computerSelection === "paper"){
-            updateScore("User");
             event.textContent = "You Win! Scissors beats Paper";
+            updateScore("User");
         }
         else if(computerSelection === "rock"){
-            updateScore("Computer");
             event.textContent = "You Lose! Rock beat Scissors";
+            updateScore("Computer");
         }
     }
     else if (playerSelection === "rock"){
         if(computerSelection === "scissors"){
-            updateScore("User");
             event.textContent = "You Win! Rock beats Scissors";
+            updateScore("User");
         }
         else if(computerSelection === "paper"){
-            updateScore("Computer");
             event.textContent = "You Lose! Paper beats Rock";
+            updateScore("Computer");
         }
     }
 }
-function reset(userScore, compScore, winner){
+function reset(winner){
+    event.textContent = (`${winner} has won the game`);
+    buttons.forEach(button => button.removeEventListener('click', playRound));
+    playAgain.classList.remove("hide");
+}
+
+function restartGame(){
+    buttons.forEach(button => button.addEventListener('click', playRound));
     userScore.textContent = "0";
     compScore.textContent = "0";
-    alert(`${winner} has won the game`);
+    event.textContent = "Game Begins"
+    playAgain.classList.add("hide")
 }
-const buttons = document.querySelectorAll('button');
-buttons.forEach(button => button.addEventListener('click', playRound));
-// function game(){
-//     for (let i = 0; i < 5; i++){
-//         let playerSelection = prompt("Select from Rock, Paper, Scissors: ");
-//         let computerSelection = computerPlay();
-//         console.log(playRound(playerSelection, computerSelection));
-//         console.log("Score: " + userScore + " vs " + compScore);
-//     }
-// }
-// game();
+
